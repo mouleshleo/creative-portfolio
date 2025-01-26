d5fb38f5f3c2381188234d889b4cda707bf20665
mouleshleo <mouleshleo06@gmail.com>
2025-01-26 12:47:46 +0530

    changed ui and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
   theme: {
     
     extend: {
+      animation: {
+        'blink': 'blink ease-in 3s infinite',
+      },
+      keyframes: {
+        blink: {
+          '50%': {borderColor: 'transparent'},
+        },
+      },
       fontSize: {
         '10xl': '15rem',
         'la': '1.1rem',
